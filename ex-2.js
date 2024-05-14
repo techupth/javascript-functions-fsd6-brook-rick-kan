e9ddd0d49f52207756865ae91b18f5e339ccbd51
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-05-14 13:55:45 +0700

    feat: js-functions-assignment

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,21 @@
 // Start coding here
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+console.log(add(10,5));
+console.log(subtract(10,5));
+console.log(multiply(10,5));
+console.log(divide(10,5));
